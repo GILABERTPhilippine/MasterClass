b0d0e97984390446a11aaf166fa84654e629cd06
PhilippineG <philippinegilabert@gmail.com>
2018-07-27 15:04:42 +0200

    modif

--- Back/trip-server/routes/image.js
+++ Back/trip-server/routes/image.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-// var path = require('path');
+var path = require('path');
 
 var Image = require('../schema/schemaImage');
 
 router.get('/', function(req, res) {
-    res.sendFile('/../trip-app/public/index.html')
+    res.sendFile(path.join(__dirname + '/../../../Front/trip-app/public/index.html'))
 })
 
 module.exports = router;
