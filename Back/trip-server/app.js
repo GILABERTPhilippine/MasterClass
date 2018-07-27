e0d7aa398fa6a057b2f05fcde7795ab79128926c
PhilippineG <philippinegilabert@gmail.com>
2018-07-27 15:02:14 +0200

    ajout schema + test route

--- Back/trip-server/app.js
+++ Back/trip-server/app.js
@@ -3,12 +3,41 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+// IMPORTANT - A ajouter
 var bodyParser = require('body-parser')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// IMPORTANT - Connection BDD
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/masterclass');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    // we're connected!
+});
+
+// var collection = db.collection('Image')
+// collection.insert({
+//     imageId: 'fgiherkdvkerhvoerhj123545',
+//     description: 'Bouuuuuuuuuu !',
+// }, function(err, result) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result)
+// })
+
 var app = express();
+
+// IMPORTANT - A ajouter
+var urlencodedParser = bodyParser.urlencoded({
+    extended: true
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,7 +45,9 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -35,8 +66,7 @@ app.use(function(err, req, res, next) {
     res.locals.error = req
         .app
         .get('env') === 'development' ?
-        err :
-        {};
+        err : {};
 
     // render the error page
     res.status(err.status || 500);
